95e5d1617a85891faa16a3901794eb80bb86cfa7
close852 <close852@naver.com>
2018-06-08 16:04:50 +0900

    pug commit

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ var express = require('express');
 var app = express();
 var path =require('path');
 var root = require(path.join(__dirname,'router','root'));
+const pug = require('pug');
+const comFn = pug.compileFile(path.join(__dirname,'views','temp.pug'));
+app.set('view engine','pug');
+app.set('views',path.join(__dirname,'views'));
 
 app.use(express.static('public'));
 app.use(root);
 
+app.get('/pug',(req,res)=>{
+    res.render('hello');
+})
 app.listen(3000,(req,res)=>{
     console.log('Connected, 3000 port!')
 }) 
