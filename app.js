d525d0524a2c31563efcbdf4eed1ae20a95a9e95
close852 <close852@naver.com>
2018-06-08 15:45:35 +0900

    static 완료 커밋

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 var path =require('path');
-var root = require(path.join(__dirname,'router','root.js'));
+var root = require(path.join(__dirname,'router','root'));
 
+app.use(express.static('public'));
 app.use(root);
+
 app.listen(3000,(req,res)=>{
     console.log('Connected, 3000 port!')
-})
+}) 
--- router/root.js
+++ router/root.js
@@ -5,4 +5,7 @@ router.get('/',(req,res)=>{
     res.send('Hello World!');
 })
 
+router.get('/route',(req,res)=>{
+    res.send('Hello Router, <img src="/img/route.png">')
+})
 module.exports = router;
